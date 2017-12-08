b97bb9dd1d6ab2ef6ab4e08c6d14eceee3b1329f
chidi79 <chidi79@users.noreply.github.com>
2017-12-08 17:16:18 +0100

    Add files via upload

---
+++ fizzbuzz.js
@@ -0,0 +1,10 @@
+for(var count = 1; count <= 100; count++){
+    if((count % 3 == 0) && (count % 5 != 0))
+        console.log("Fizz");
+    else if((count % 5 == 0) && (count % 3 != 0))
+        console.log("Buzz");
+    else if((count % 5 == 0) && (count % 3 == 0))
+    console.log("FizzBuzz");
+    else
+        console.log(count);
+}
---
+++ helloWorld.js
@@ -0,0 +1,3 @@
+for (var count = "#"; count.length < 8; count+="#"){
+    console.log(count);
+}
