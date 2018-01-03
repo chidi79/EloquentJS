5ade9754a4139e5c7502b2b8beaff1ea3dfc7e27
egusiboy79 <egusiboy79@gmail.com>
2018-01-03 08:27:35 +0100

    updates from Jan 2 2018

--- chap04/helloWorld.js
+++ chap04/helloWorld.js
@@ -2,15 +2,18 @@ var listOfOddNumbers = [1,3,5,7,9];
 console.log("listOfOddNumbers: "+listOfOddNumbers);
 console.log("listOfOddNumbers has length: "+listOfOddNumbers.length);
 console.log("listOfOddNumbers has length "+listOfOddNumbers["length"]);
+console.log("\n");
 
-var myArr = [];
+var myArr = ["rich","poor","house","field"];
 myArr.push("light", "dark", "faux", "real");
 
 console.log(myArr);
 console.log("myArr.join(): "+myArr.join("$"));
+console.log("\n");
 
 myArr.pop();
 console.log(myArr);
+console.log("\n");
 
 var daysOfTheWeek = {
     "workDays" : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
@@ -18,6 +21,11 @@ var daysOfTheWeek = {
 };
 
 console.log(daysOfTheWeek.workDays);
-
 console.log(daysOfTheWeek.workDays.indexOf("Saturday"));
+console.log("weekdays" in daysOfTheWeek);
+console.log("\n");
+
+for (var weekDay in daysOfTheWeek){
+    console.log("the entries in "+ weekDay +" are: " +daysOfTheWeek[weekDay]);
+}
 
